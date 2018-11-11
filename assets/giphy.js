c1b9e4e70d9a564eb6c457e24c66d8402348ba8a
divya <gopalakrishnandivya6@gmail.com>
2018-11-11 12:33:27 -0700

    3commit

--- assets/giphy.js
+++ assets/giphy.js
@@ -4,11 +4,18 @@ $(document).ready(function () {
     var topics = ["Boxing", "Soccer", "Baseball", "Ice hockey", "Badminton", "Basketball", "Mind sport", "Tennis", "Rowing", "Bowling", "Table tennis"];
     // apikey=WHFMIfploVoswYVxgob2X3QZdEHAgXyX;
     // curl "http://api.giphy.com/v1/gifs/search?q=cricket&api_key=WHFMIfploVoswYVxgob2X3QZdEHAgXyX&limit=10"
-function displaySportsinfo() {
-    var sport= $(this).attr("data-name");
-    var queryURL=
+    function displaySportsinfo() {
+        var sport = $(this).attr("data-name");
 
-}
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q="+ sport +" &api_key=WHFMIfploVoswYVxgob2X3QZdEHAgXyX&limit=10";
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            $("#sport-view").text(JSON.stringify(response));
+        });
+
+    }
 
 
 
@@ -29,6 +36,20 @@ function displaySportsinfo() {
 
     }
 
+
+
+    $("#add-sportitem").on("click", function(event) {
+        event.preventDefault();
+
+        var sport = $("#sport-input").val().trim();
+
+        topics.push(sport);
+        console.log(topics);
+
+        // Calling renderButtons which handles the processing of our movie array
+        renderButtons();
+      });
+      $(document).on("click", ".sport", displaySportsinfo );
 
     renderButtons();
 });
