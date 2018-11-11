e03e7ee32b21b376d694f27295d24df2c6406f55
divya <gopalakrishnandivya6@gmail.com>
2018-11-11 11:46:22 -0700

    2 commit

--- assets/giphy.js
+++ assets/giphy.js
@@ -1,5 +1,23 @@
 
 
-// Initial array of 
-var movies = ["Dog", "Rabbit", "cat", "Bird","Frog","Hamster","chicken",];
-apikey=WHFMIfploVoswYVxgob2X3QZdEHAgXyX;
+// Initial array of sports
+var topics = ["Boxing","Soccer","Baseball","Ice hockey","Badminton","Basketball","Mind sport","Tennis","Rowing","Bowling","Table tennis"];
+// apikey=WHFMIfploVoswYVxgob2X3QZdEHAgXyX;
+// curl "http://api.giphy.com/v1/gifs/search?q=cricket&api_key=WHFMIfploVoswYVxgob2X3QZdEHAgXyX&limit=10"
+
+    function renderButtons() {
+        var sports=$("<button>");
+        sports.addClass("sport");
+        sports.attr("data-name",topics[i]);
+        sports.text(topics[i]);
+        $("#buttons-view").append( sports);
+
+
+
+
+
+
+
+    }
+
+
